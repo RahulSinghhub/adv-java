3dc676c8c84c790d68e2c42eb77611663b162c0a
Rahul Singh <p1273105@gmail.com>
2024-06-24 23:20:10 +0530

    Update Utils.java

--- vechicle/src/com/app/utils/Utils.java
+++ vechicle/src/com/app/utils/Utils.java
@@ -4,7 +4,7 @@ import com.app.vechicle.Custom_Exception;
 
 public class Utils {
 	public static void valPoll(double p) throws Custom_Exception {
-		if (p>20)
+		if (p>20)//for all vechicles  whose po;;ution level is high
 			throw new Custom_Exception("pollution level is high");
 			}
 }
