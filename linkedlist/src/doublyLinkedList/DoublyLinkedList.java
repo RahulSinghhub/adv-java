7a7d7be911e91e4d9c10705926ff8adc7e55293a
RahulSinghhub <p1273105@gmial.com>
2024-06-26 21:16:56 +0530

    data structures

--- linkedlist/src/doublyLinkedList/DoublyLinkedList.java
+++ linkedlist/src/doublyLinkedList/DoublyLinkedList.java
@@ -1,5 +1,19 @@
 package doublyLinkedList;
 
 public class DoublyLinkedList {
+	private Node head;
+	public DoublyLinkedList() {
+		this.head=null;
+	}
+	public boolean insert(int data) {
+		Node newNode = new Node(data);
+		if(newNode==null) {
+			
+		}
+		if(head==null) {
+			return 
+		}
+		return true;
+	}
 
 }
